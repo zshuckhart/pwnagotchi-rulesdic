191aeea87c58740c44961aa253106d4e5106ceea
aww_shucks <zach.shuckhart@gmail.com>
2025-03-19 13:29:29 -0700

    Update rulesdic.py

--- rulesdic.py
+++ rulesdic.py
@@ -80,23 +80,23 @@ TEMPLATE = """
 {% block script %}
     var searchInput = document.getElementById('searchText');
     searchInput.onkeyup = function() {
-        var filter = searchInput.value.toUpperCase();
-        var table = document.getElementById('tableOptions');
-        if (table) {
-            var tr = table.getElementsByTagName('tr');
-            for (var i = 0; tr.length; i++) {
-                var td = tr[i].getElementsByTagName('td')[0];
-                if (td) {
-                    var txtValue = td.textContent || td.innerText;
-                    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                        tr[i].style.display = '';
-                    } else {
-                        tr[i].style.display = 'none';
-                    }
+    var filter = searchInput.value.toUpperCase();
+    var table = document.getElementById('tableOptions');
+    if (table) {
+        var tr = table.getElementsByTagName('tr');
+        for (var i = 0; i < tr.length; i++) {
+            var td = tr[i].getElementsByTagName('td')[0];
+            if (td) {
+                var txtValue = td.textContent || td.innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = '';
+                } else {
+                    tr[i].style.display = 'none';
                 }
             }
         }
     }
+}
 {% endblock %}
 {% block content %}
     <input type="text" id="searchText" placeholder="Search for ..." title="Type in a filter">
